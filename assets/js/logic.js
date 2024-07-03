8745074f508d4b7501eb1855b453796d1f116bcf
egodschalk <egodschalk@gmail.com>
2024-07-03 02:19:24 -0400

    toggle dark modeedits

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,21 @@
 // when user clicks on the sun/moon button. If in lt mode, switch to dk mode, if in dk mode, switch to lt mode
 // 1. grab ref to html from button
-// const modeEl = document.querySelector(".mode");
+const modeEl = document.getElementById('mode');
+const dkBody = document.querySelector('.dkbody');
+const dkHeader = document.querySelector('.dkheader');
+const dkButton = document.querySelector('.dkbutton');
+const dkForm = document.querySelector('.dkform');
+const dkSubmit = document.querySelector('.dksubmit');
 
-// 2. way to switch modes- switch between 2 css classes
-document.body.classList.toggle('dark-mode');
+// Switch  between modes (switch between 2 css classes)
+// classList.toggle properties taken from learning assistant
+modeEl.addEventListener('click', function() {
+    dkBody.classList.toggle('dark-body');
+    dkHeader.classList.toggle('dark-header');
+    dkButton.classList.toggle('dark-btn');
+    dkForm.classList.toggle('dark-form');
+    dkSubmit.classList.toggle('dark-submit');
+});
 
 
 // 3. remember mode preference (local storage, do later)
