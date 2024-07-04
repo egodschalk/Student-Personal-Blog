5c81c5247cb3679dd70022b3d06b14b9d1d95f6b
egodschalk <egodschalk@gmail.com>
2024-07-04 01:54:19 -0400

    fixed posting functionality, back button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -13,35 +13,41 @@ modeEl.addEventListener('click', function() {
 // back button needed
 
 // Pull in form submissions and create elements
+let blogArray = JSON.parse(localStorage.getItem('blogPost'));
 
 const blogTitle = document.getElementById('blog-title');
 const blogContent = document.getElementById('blog-content');
 const blogAuthor = document.getElementById('blog-author');
+const blogPostsContainer = document.getElementById('blogposts');
 
 console.log(blogTitle);
 console.log(blogContent);
 console.log(blogAuthor);
 
-
+console.log('blogArray:',blogArray)
 
 // view list of blog posts pulled from local storage
 // Got help with how to display the array of abjects on page from class instructor
 for (let i = 0; i < blogArray.length; i++) {
     const  blogCard = document.createElement("div");
-    blogCard.setAttribute("class", "card");
+    blogCard.setAttribute("class", "card dkcard");
 
   // each blog entry on page shows title, content, author
     const blogTitleH2 = document.createElement("h2");
     blogTitleH2.textContent = blogArray[i].blogTitle;
+    blogTitleH2.setAttribute("class", "dkh2");
     blogCard.append(blogTitleH2);
 
     const blogContentP = document.createElement("p");
     blogContentP.textContent = blogArray[i].blogContent;
+    blogContentP.setAttribute("id", "blog-content");
     blogCard.append(blogContentP);
 
     const blogAuthorP = document.createElement("p");
     blogAuthorP.textContent = blogArray[i].blogAuthor;
+    blogAuthorP.setAttribute("id", "blog-author");
     blogCard.append(blogAuthorP);
 
+    blogPostsContainer.append(blogCard);
 }
 
